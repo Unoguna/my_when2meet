05f8606531f738e5d4b5aa19a3fba70ed56f7b81
Jeongho Kim <kimjungho.kr@gmail.com>
2025-01-30 17:31:41 +0900

    first commit

---
+++ src/main/java/my_when2meet/my_when2meet_spring/MyWhen2meetSpringApplication.java
@@ -0,0 +1,13 @@
+package my_when2meet.my_when2meet_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWhen2meetSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWhen2meetSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my_when2meet/my_when2meet_spring/MyWhen2meetSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package my_when2meet.my_when2meet_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWhen2meetSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
