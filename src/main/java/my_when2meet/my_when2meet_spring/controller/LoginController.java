8205d8e2b80db250380d4c30558cfbae26fd0230
Jeongho Kim <kimjungho.kr@gmail.com>
2025-01-30 18:10:04 +0900

    LoginController location change

--- src/main/java/my_when2meet/my_when2meet_spring/controller/LoginController.java
+++ src/main/java/my_when2meet/my_when2meet_spring/controller/LoginController.java
@@ -1,4 +1,4 @@
-package controller;
+package my_when2meet.my_when2meet_spring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
