89f5b77e5f0671165a6b7e665e6c7810bc767e0f
Jeongho Kim <kimjungho.kr@gmail.com>
2025-01-31 16:23:04 +0900

    add member domain

---
+++ src/main/java/my_when2meet/my_when2meet_spring/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package my_when2meet.my_when2meet_spring.repository;
+
+import my_when2meet.my_when2meet_spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+}
