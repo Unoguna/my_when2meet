0c0c882a1feed8ed6f59915c0de5ebbcf9b0ea77
Jeongho Kim <kimjungho.kr@gmail.com>
2025-02-04 16:39:01 +0900

    connect to H2 database && Test success

--- src/main/java/my_when2meet/my_when2meet_spring/Config.java
+++ src/main/java/my_when2meet/my_when2meet_spring/Config.java
@@ -1,13 +1,22 @@
 package my_when2meet.my_when2meet_spring;
 
+import my_when2meet.my_when2meet_spring.repository.JdbcTemplateMemberRepository;
 import my_when2meet.my_when2meet_spring.repository.MemberRepository;
 import my_when2meet.my_when2meet_spring.repository.MemoryMemberRepository;
 import my_when2meet.my_when2meet_spring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class Config {
+
+    private final DataSource dataSource;
+
+    public Config(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +25,6 @@ public class Config {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
---
+++ src/main/java/my_when2meet/my_when2meet_spring/repository/JdbcTemplateMemberRepository.java
@@ -0,0 +1,72 @@
+package my_when2meet.my_when2meet_spring.repository;
+
+import my_when2meet.my_when2meet_spring.domain.Member;
+import org.springframework.boot.autoconfigure.batch.BatchProperties;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource; import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class JdbcTemplateMemberRepository implements MemberRepository{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplateMemberRepository(DataSource dataSource){
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("member").usingGeneratedKeyColumns("userid");
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("id", member.getId());
+        parameters.put("password", member.getPassword());
+        parameters.put("name", member.getName());
+
+
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+        member.setUserid(key.longValue());
+
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findByUserId(Long userid) {
+        List<Member> result = jdbcTemplate.query("select * from member where userid = ?", memberRowMapper(), userid);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public Optional<Member> findById(String id) {
+        List<Member> result = jdbcTemplate.query("select * from member where id = ?", memberRowMapper(), id);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findByName(String name) {
+        return jdbcTemplate.query("select * from member where name = ?", memberRowMapper(), name);
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return jdbcTemplate.query("select * from member", memberRowMapper());
+    }
+
+    private RowMapper<Member> memberRowMapper() {
+        return (rs, rowNum) -> {
+            Member member = new Member();
+            member.setUserid(rs.getLong("userid"));
+            member.setId(rs.getString("id"));
+            member.setPassword(rs.getString("password"));
+            member.setName(rs.getString("name"));
+            return member;
+        };
+    }
+}
